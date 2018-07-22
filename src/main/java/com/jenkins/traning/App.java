a18f79d61c6c54858242d5e4c495a8ab1ad55275
xuchengyin <xuchengyin@uinnova.com>
2018-07-22 21:37:45 +0800

    add test class

---
+++ src/main/java/com/jenkins/traning/App.java
@@ -0,0 +1,17 @@
+package com.jenkins.traning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public int sum(int a,int b) {
+    	int c=a+b;
+    	return c;
+    }
+    public int sub(int a,int b) {
+    	int c=a-b;
+    	return c;
+    }
+}
